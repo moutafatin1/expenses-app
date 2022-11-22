307955a8b725ee3250ce4c2e43ad617d91c88f61
Oussama Moutafatin <moutafatin3@gmail.com>
2022-11-22 18:09:18 +0100

    little tests

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -7,16 +7,16 @@ import type { NextPageWithLayout } from "../_app";
 const DashboardPage: NextPageWithLayout = () => {
   return (
     <div className="h-full  p-8">
-      <div className="flex items-center gap-4">
-        <div className="h-40 flex-1 rounded-md bg-gray-300"></div>
-        <div className="h-40 flex-1 rounded-md bg-gray-300"></div>
-        <div className="h-40 flex-1 rounded-md bg-gray-300"></div>
+      <div className="flex flex-wrap  items-center gap-4">
+        <div className="h-40  flex-1 rounded-md bg-gray-300"></div>
+        <div className="h-40  flex-1 rounded-md bg-gray-300"></div>
+        <div className="h-40  flex-1 rounded-md bg-gray-300"></div>
       </div>
-      <div className="mt-16 flex items-center gap-4">
-        <div className="h-96 w-1/3 border-2">
+      <div className="mt-12 flex flex-col items-center gap-4 lg:flex-row">
+        <div className="h-96 w-full  border-2 lg:w-1/2">
           <PieChart />
         </div>
-        <div className="h-96 w-full">
+        <div className="h-96 w-full min-w-0">
           <LineChart />
         </div>
       </div>
