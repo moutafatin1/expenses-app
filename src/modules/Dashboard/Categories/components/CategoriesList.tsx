e30f6588c5d26980950275e01192fb977dd26965
Oussama Moutafatin <moutafatin3@gmail.com>
2022-12-05 07:54:32 +0100

    change search bar position

--- src/modules/Dashboard/Categories/components/CategoriesList.tsx
+++ src/modules/Dashboard/Categories/components/CategoriesList.tsx
@@ -6,6 +6,7 @@ import { getPaginationMetadata } from "@modules/common/utils";
 import type { Category } from "@prisma/client";
 import { trpc } from "@utils/trpc";
 import { useEffect, useState } from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 import { CategoryRow } from "./CategoryRow";
 
 type CategoriesListProps = {
@@ -50,55 +51,59 @@ export const CategoriesList = ({ openUpdateDialog }: CategoriesListProps) => {
   };
 
   return (
-    <div className="relative -mx-4 mt-8 overflow-hidden rounded-xl  shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 ">
+    <>
       <InputField
-        className="absolute top-1 right-1 z-20 w-24 sm:w-36 lg:w-64"
+        className="w-24 sm:w-36 lg:w-64"
         placeholder="Search..."
+        label="Search"
         value={search}
+        startIcon={<AiOutlineSearch />}
         onChange={(e) => setSearch(e.target.value)}
       />
-      <table className="min-w-full divide-y divide-gray-300 ">
-        <thead className="bg-gray-50">
-          <tr>
-            <th
-              scope="col"
-              className="py-3.5 pl-5 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-8"
-            >
-              Emoji
-            </th>
-            <th
-              scope="col"
-              className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
-            >
-              Name
-            </th>
+      <div className="-mx-4 mt-4 overflow-hidden rounded-xl  shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 ">
+        <table className="min-w-full divide-y divide-gray-300 ">
+          <thead className="bg-gray-50">
+            <tr>
+              <th
+                scope="col"
+                className="py-3.5 pl-5 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-8"
+              >
+                Emoji
+              </th>
+              <th
+                scope="col"
+                className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 "
+              >
+                Name
+              </th>
 
-            <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
-              <span className="sr-only">Edit</span>
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200 bg-white">
-          {data.categories.map((category) => (
-            <CategoryRow
-              openUpdateDialog={openUpdateDialog}
-              key={category.id}
-              category={category}
-            />
-          ))}
-        </tbody>
-      </table>
-      <Pagination
-        isPrevious={page !== 1}
-        isNext={data.hasMore}
-        metaData={{
-          endIndex,
-          startIndex,
-          total: data.totalRowCount,
-        }}
-        nextPage={setNextPage}
-        previousPage={setPreviousPage}
-      />
-    </div>
+              <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200 bg-white">
+            {data.categories.map((category) => (
+              <CategoryRow
+                openUpdateDialog={openUpdateDialog}
+                key={category.id}
+                category={category}
+              />
+            ))}
+          </tbody>
+        </table>
+        <Pagination
+          isPrevious={page !== 1}
+          isNext={data.hasMore}
+          metaData={{
+            endIndex,
+            startIndex,
+            total: data.totalRowCount,
+          }}
+          nextPage={setNextPage}
+          previousPage={setPreviousPage}
+        />
+      </div>
+    </>
   );
 };
--- src/modules/common/components/Forms/InputWrapper.tsx
+++ src/modules/common/components/Forms/InputWrapper.tsx
@@ -25,16 +25,16 @@ export const InputWrapper = ({
   return (
     <label className="w-full space-y-1">
       {label && (
-        <span className="text-sm font-medium capitalize text-gray-700">
+        <span className="mb-1 block text-sm font-medium capitalize text-gray-700">
           {label} {required && "*"}
         </span>
       )}
-      <div className="relative rounded-md shadow-sm">
+      <span className="relative rounded-md shadow-sm">
         {/* Leading Icon */}
         {startIcon && (
-          <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-xl text-gray-600">
+          <span className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3 text-xl text-gray-600">
             {startIcon}
-          </div>
+          </span>
         )}
 
         {/* Input */}
@@ -42,18 +42,18 @@ export const InputWrapper = ({
 
         {/* Trailing Icon */}
         {endIcon && !errorMessage && (
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-gray-600">
+          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-gray-600">
             {endIcon}
-          </div>
+          </span>
         )}
 
         {/* Error Icon */}
         {errorMessage && (
-          <div className="absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-red-500">
+          <span className="absolute inset-y-0 right-0 flex items-center pr-3 text-xl text-red-500">
             <AiFillExclamationCircle className="" />
-          </div>
+          </span>
         )}
-      </div>
+      </span>
 
       {/* Error */}
       {errorMessage && (
